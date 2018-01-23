ae406e055933f52763ebb8cae9703cffe2ef8b10
Christina Moore <sayhitochristina@gmail.com>
2018-01-23 15:27:22 -0600

    working as intended

--- scripts/index.js
+++ scripts/index.js
@@ -5,37 +5,6 @@
 
 const API_KEY = 'AIzaSyDbHRxDgCEttJI4x0R7drtclNmnaPVmzEE';
 
-const mockData = [{
-	id: "hY7m5jjJ9mM",
-	thumbnail: "https://i.ytimg.com/vi/hY7m5jjJ9mM/hqdefault.jpg",
-	title: "CATS will make you LAUGH YOUR HEAD OFF - Funny CAT compilation",
-},
-
-{
-	id: "5dsGWM5XGdg",
-	thumbnail: "https://i.ytimg.com/vi/5dsGWM5XGdg/hqdefault.jpg",
-	title: "Cats are so funny you will die laughing - Funny cat compilation",
-},
-
-{
-	id: "WEkSYw3o5is",
-	thumbnail: "https://i.ytimg.com/vi/WEkSYw3o5is/hqdefault.jpg",
-	title: "You will LAUGH SO HARD that YOU WILL FAINT - FUNNY CAT compilation",
-},
-
-{
-	id: "vcrjuOH5R3A",
-	thumbnail: "https://i.ytimg.com/vi/vcrjuOH5R3A/hqdefault_live.jpg",
-	title: "Cat Music Live TV / Non Stop Music 24/7 🎧",
-},
-
-{
-	id: "opYts0nFOvs",
-	thumbnail: "https://i.ytimg.com/vi/opYts0nFOvs/hqdefault.jpg",
-	title: "NINJA CATS! There's absolutely NOTHING MORE FUNNY!  - Impossible TRY NOT TO LAUGH compilation",
-},
-];
-
 const store = {
 	videos: []
 };
@@ -58,6 +27,7 @@ const BASE_URL = 'https://www.googleapis.com/youtube/v3/search';
 // 3. Make a getJSON call using the query object and sending the provided callback in as the last argument
 // TEST IT! Execute this function and console log the results inside the callback.
 const fetchVideos = function(searchTerm, callback) {
+  //debugger;
 	const request = {
 		part: 'snippet',
 		key: API_KEY,
@@ -79,13 +49,14 @@ const fetchVideos = function(searchTerm, callback) {
 const decorateResponse = function(data) {
 	console.log('hello this is decorate response');
 	// const results = data.items.map((item, index) => renderResult(item));
-	const createItem = data.items.map(function(item) {
+	const videos = data.items.map(function(item) {
 		return {
 			id : item.id.videoId,
 			title : item.snippet.title,
 			thumbnail : item.snippet.thumbnails.high.url,
-		};
-	});
+    };
+  });
+   addVideosToStore(videos);
 	// console.log(createItem);
 	
 };
@@ -95,12 +66,12 @@ const decorateResponse = function(data) {
 // 2. Using the object, return an HTML string containing all the expected data
 // TEST IT!
 const generateVideoItemHtml = function(video) {
-	console.log('generateVideoItemHtml');
-	return `
+  console.log('generateVideoItemHtml');
+  //	<span>${video.id}</span>
+  return `
 	<li>
-		<span>${video.id}</span>
-		<span>${video.title}</span>		
-		<span>${video.thumbnail}</span>
+		<h2>${video.title}</h2>		
+		<img src = ${video.thumbnail}></image>
 	</li>
 	`
 	
@@ -111,9 +82,9 @@ const generateVideoItemHtml = function(video) {
 // objects and sets the array as the value held in store.items
 // TEST IT!
 const addVideosToStore = function(videos) {
-	store.videos = videos; 
-	console.log(store.videos);
-	
+  console.log('add videos ran');
+  store.videos = videos; 
+  render();
 };
 
 // TASK:
@@ -122,10 +93,11 @@ const addVideosToStore = function(videos) {
 // 3. Add your array of DOM elements to the appropriate DOM element
 // TEST IT!
 const render = function() {
-	const results = store.videos.map((item, index) => generateVideoItemHtml(item));
-	// console.log(results);
-	
-	$('.results').html(results);
+ //debugger;
+  console.log('inside render' , store);
+  const results = store.videos.map((item, index) => generateVideoItemHtml(item));
+  $('.results').html(results);
+  console.log('render html');
 };
 
 // TASK:
@@ -135,18 +107,20 @@ const render = function() {
 //   b) Retrieve the search input from the DOM
 //   c) Clear the search input field
 //   d) Invoke the `fetchVideos` function, sending in the search value
-//   e) Inside the callback, send the API response through the `decorateResponse` function
-//   f) Inside the callback, add the decorated response into your store using the `addVideosToStore` function
-//   g) Inside the callback, run the `render` function 
+//   e) Inside the callback(original callback), send the API response through the `decorateResponse` function
+//   f) Inside the callback(DecorateResponses), add the decorated response into your store using the `addVideosToStore` function
+//   g) Inside the callback(AddVideosToStore), run the `render` function 
 // TEST IT!
 const handleFormSubmit = function() {
 	$('#js-search-form').submit(event => {
 		event.preventDefault();
 		console.log('Executing handle');
 		// debugger;
-		const searchTerm = $('#search-term').val();
+    const searchQuery = $(event.currentTarget).find('.search-term');
+    const searchTerm = searchQuery.val();
 		console.log(searchTerm);
-		searchTerm.val("");
+    searchQuery.val("");
+    fetchVideos(searchTerm, decorateResponse);
 	}
 	)
 };
@@ -155,9 +129,6 @@ const handleFormSubmit = function() {
 $(function () {
 	// TASK:
 	// 1. Run `handleFormSubmit` to bind the event listener to the DOM
-	fetchVideos(sampleQuery, decorateResponse);
-	// generateVideoItemHtml(mockData);
-	addVideosToStore(mockData);
-	render();
-	handleFormSubmit();
+  handleFormSubmit();
+ 
 });
