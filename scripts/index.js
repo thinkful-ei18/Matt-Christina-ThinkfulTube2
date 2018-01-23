0d849aae349aa2a5ee4a36bc80c38263315e626e
mattNat <mattsanpedro76@gmail.com>
2018-01-23 11:54:55 -0800

    need debug <li> item

--- scripts/index.js
+++ scripts/index.js
@@ -5,6 +5,37 @@
 
 const API_KEY = 'AIzaSyDbHRxDgCEttJI4x0R7drtclNmnaPVmzEE';
 
+const mockData = [{
+	id: "hY7m5jjJ9mM",
+	thumbnail: "https://i.ytimg.com/vi/hY7m5jjJ9mM/hqdefault.jpg",
+	title: "CATS will make you LAUGH YOUR HEAD OFF - Funny CAT compilation",
+},
+
+{
+	id: "5dsGWM5XGdg",
+	thumbnail: "https://i.ytimg.com/vi/5dsGWM5XGdg/hqdefault.jpg",
+	title: "Cats are so funny you will die laughing - Funny cat compilation",
+},
+
+{
+	id: "WEkSYw3o5is",
+	thumbnail: "https://i.ytimg.com/vi/WEkSYw3o5is/hqdefault.jpg",
+	title: "You will LAUGH SO HARD that YOU WILL FAINT - FUNNY CAT compilation",
+},
+
+{
+	id: "vcrjuOH5R3A",
+	thumbnail: "https://i.ytimg.com/vi/vcrjuOH5R3A/hqdefault_live.jpg",
+	title: "Cat Music Live TV / Non Stop Music 24/7 🎧",
+},
+
+{
+	id: "opYts0nFOvs",
+	thumbnail: "https://i.ytimg.com/vi/opYts0nFOvs/hqdefault.jpg",
+	title: "NINJA CATS! There's absolutely NOTHING MORE FUNNY!  - Impossible TRY NOT TO LAUGH compilation",
+},
+];
+
 const store = {
 	videos: []
 };
@@ -33,6 +64,8 @@ const fetchVideos = function(searchTerm, callback) {
 		q: `${searchTerm}`,
 	};
 	$.getJSON(BASE_URL, request, callback);
+	
+	
 };
 
 // TASK:
@@ -46,13 +79,15 @@ const fetchVideos = function(searchTerm, callback) {
 const decorateResponse = function(data) {
 	console.log('hello this is decorate response');
 	// const results = data.items.map((item, index) => renderResult(item));
-	return data.items.map(function(item) {
+	const createItem = data.items.map(function(item) {
 		return {
 			id : item.id.videoId,
 			title : item.snippet.title,
 			thumbnail : item.snippet.thumbnails.high.url,
 		};
 	});
+	// console.log(createItem);
+	
 };
 
 // TASK:
@@ -60,7 +95,13 @@ const decorateResponse = function(data) {
 // 2. Using the object, return an HTML string containing all the expected data
 // TEST IT!
 const generateVideoItemHtml = function(video) {
-
+	console.log('generateVideoItemHtml');
+	return `
+	<li>
+	<span>${video.thumbnail}</span>
+	<\li>
+	`
+	
 };
 
 // TASK:
@@ -68,7 +109,9 @@ const generateVideoItemHtml = function(video) {
 // objects and sets the array as the value held in store.items
 // TEST IT!
 const addVideosToStore = function(videos) {
-
+	store.videos = videos; 
+	console.log(store.videos);
+	
 };
 
 // TASK:
@@ -77,7 +120,10 @@ const addVideosToStore = function(videos) {
 // 3. Add your array of DOM elements to the appropriate DOM element
 // TEST IT!
 const render = function() {
-
+	const results = store.videos.map((item, index) => generateVideoItemHtml(item));
+	// console.log(results);
+	
+	$('.results').html(results);
 };
 
 // TASK:
@@ -100,5 +146,7 @@ $(function () {
 	// TASK:
 	// 1. Run `handleFormSubmit` to bind the event listener to the DOM
 	fetchVideos(sampleQuery, decorateResponse);
-  
+	// generateVideoItemHtml(mockData);
+	addVideosToStore(mockData);
+	render();
 });
---
+++ scripts/test.js
@@ -0,0 +1,35 @@
+const mockData = [{
+    id: "hY7m5jjJ9mM",
+    thumbnail: "https://i.ytimg.com/vi/hY7m5jjJ9mM/hqdefault.jpg",
+    title: "CATS will make you LAUGH YOUR HEAD OFF - Funny CAT compilation",
+  },
+
+  {
+    id: "5dsGWM5XGdg",
+    thumbnail: "https://i.ytimg.com/vi/5dsGWM5XGdg/hqdefault.jpg",
+    title: "Cats are so funny you will die laughing - Funny cat compilation",
+
+  },
+
+  {
+    id: "WEkSYw3o5is",
+    thumbnail: "https://i.ytimg.com/vi/WEkSYw3o5is/hqdefault.jpg",
+    title: "You will LAUGH SO HARD that YOU WILL FAINT - FUNNY CAT compilation",
+
+  },
+
+  {
+    id: "vcrjuOH5R3A",
+    thumbnail: "https://i.ytimg.com/vi/vcrjuOH5R3A/hqdefault_live.jpg",
+    title: "Cat Music Live TV / Non Stop Music 24/7 🎧",
+
+  },
+
+  {
+    id: "opYts0nFOvs",
+    thumbnail: "https://i.ytimg.com/vi/opYts0nFOvs/hqdefault.jpg",
+    title: "NINJA CATS! There's absolutely NOTHING MORE FUNNY!  - Impossible TRY NOT TO LAUGH compilation",
+
+  },
+
+];
